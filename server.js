b038778966cfcf268ac88bb8dee186a9ff5fec29
gleidsolaire <shuigo.3101@gmail.com>
2020-08-06 19:13:41 -0300

    Deploy Heroku 2.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const proffys = [
     {
         name: "Gisele",
         avatar: "https://avatars1.githubusercontent.com/u/26143356?s=460&u=bd913206764b35dfa365b3226f84f896b161e7d6&v=4",
-        whatsapp: " 41 992279790",
+        whatsapp: " 41 994343434",
         bio: "blablablablablblablbala",
         subject:"Pedagogia",
         cost: "20",
